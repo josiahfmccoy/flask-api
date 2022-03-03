afdaf375e7bb92d629d8dbc0ae80a1a4afcefe1f
josiahfmccoy <josiahfmccoy@gmail.com>
2022-03-03 09:33:10 -0500

    Bugfix in app route creation

--- flask_api/routes.py
+++ flask_api/routes.py
@@ -11,55 +11,55 @@ def create_generic_api_routes(api, app):
     def err_api(error):
         return error.to_response(serializer=api.serializer)
 
-        @app.route("/api/")
-        def api_map():
-            try:
-                api_urls = defaultdict(dict)
-                for rule in app.url_map.iter_rules():
-                    url = rule.rule
-                    if not url.startswith('/api'):
+    @app.route("/api/")
+    def api_map():
+        try:
+            api_urls = defaultdict(dict)
+            for rule in app.url_map.iter_rules():
+                url = rule.rule
+                if not url.startswith('/api'):
+                    continue
+                if rule.endpoint.endswith('404'):
+                    continue
+                if rule.endpoint.endswith('_redirect'):
+                    continue
+                for h in api.hidden_routes:
+                    if url.startswith(h):
                         continue
-                    if rule.endpoint.endswith('404'):
-                        continue
-                    if rule.endpoint.endswith('_redirect'):
-                        continue
-                    for h in api.hidden_routes:
-                        if url.startswith(h):
-                            continue
-                    endpoint = rule.endpoint.split('.', 1)
-                    if len(endpoint) == 1:
-                        api_urls[rule.endpoint] = {
-                            'url': url,
-                            'methods': list(rule.methods)
-                        }
-                    else:
-                        api_urls[endpoint[0]][endpoint[1]] = {
-                            'url': url,
-                            'methods': list(rule.methods)
-                        }
-                return ApiResult({'endpoints': api_urls})
-            except Exception as e:
-                return ApiException(str(e))
+                endpoint = rule.endpoint.split('.', 1)
+                if len(endpoint) == 1:
+                    api_urls[rule.endpoint] = {
+                        'url': url,
+                        'methods': list(rule.methods)
+                    }
+                else:
+                    api_urls[endpoint[0]][endpoint[1]] = {
+                        'url': url,
+                        'methods': list(rule.methods)
+                    }
+            return ApiResult({'endpoints': api_urls})
+        except Exception as e:
+            return ApiException(str(e))
 
-        @app.route('/api/job/<string:job_id>', methods=['GET'])
-        def check_job(job_id):
-            app.logger.debug(f'Checking for job {job_id} ...')
-            try:
-                fpath = job_path(job_id)
-                with open(fpath, 'r') as f:
-                    data = json.load(f)
-            except FileNotFoundError:
-                return ApiResult({'status': 'pending'})
+    @app.route('/api/job/<string:job_id>', methods=['GET'])
+    def check_job(job_id):
+        app.logger.debug(f'Checking for job {job_id} ...')
+        try:
+            fpath = job_path(job_id)
+            with open(fpath, 'r') as f:
+                data = json.load(f)
+        except FileNotFoundError:
+            return ApiResult({'status': 'pending'})
 
-            try:
-                os.remove(fpath)
-            except Exception:
-                pass
+        try:
+            os.remove(fpath)
+        except Exception:
+            pass
 
-            return ApiResult(data)
+        return ApiResult(data)
 
-        @app.route("/api/<path:dummy>/")
-        def api_404(dummy=None, methods=[
-            'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
-        ]):
-            raise ApiException('Not Found', 404)
+    @app.route("/api/<path:dummy>/")
+    def api_404(dummy=None, methods=[
+        'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'
+    ]):
+        raise ApiException('Not Found', 404)
