bba6567067e831347b8f1771ddca8bcb781aa9e4
josiahfmccoy <josiahfmccoy@gmail.com>
2023-09-20 09:58:40 -0400

    Catch PyJWT not installed and disable get_jwt

--- flask_api/utils/validation.py
+++ flask_api/utils/validation.py
@@ -1,10 +1,20 @@
-import jwt
+try:
+    import jwt
+except Exception:
+    import warnings
+    warnings.warn(
+        'PyJWT is not installed; JWT authentication is disabled',
+        ImportWarning
+    )
+    jwt = None
 from flask import request
 
 __all__ = ['get_jwt']
 
 
 def get_jwt(jwt_name='token', decoded=False, algorithms=['HS512']):
+    if jwt is None:
+        return None
 
     token = None
     try:
